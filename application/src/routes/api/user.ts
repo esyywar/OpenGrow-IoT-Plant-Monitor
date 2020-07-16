f20c89beb1ac1c5427e8958d5f74a373725ecc44
esyywar <rahuleswar@gmail.com>
2020-07-16 02:06:07 -0400

    user login added

--- application/src/routes/api/user.ts
+++ application/src/routes/api/user.ts
@@ -8,6 +8,7 @@ import { check, validationResult } from 'express-validator'
 import User, { IUser } from '../../models/User'
 
 import jwt from 'jsonwebtoken'
+import { userInfo } from 'os'
 
 const router = express.Router()
 
@@ -39,7 +40,7 @@ router.post(
 			return res.status(400).json({ errors: errors.array() })
 		}
 
-		/* Registering user ... */
+		/* Extract from reqest */
 		const { username, email, password } = req.body
 
 		try {
@@ -52,10 +53,8 @@ router.post(
 
 			let user: IUser = new User({ email, username })
 
-			let hashPassword = bcrypt.hashSync(password, 10)
-			user.password = hashPassword
+			user.password = await bcrypt.hash(password, 10)
 
-			/* Save new user in the databse */
 			await user.save()
 
 			const payload = {
@@ -70,16 +69,66 @@ router.post(
 				config.get('jwt.jwtSecret'),
 				{ expiresIn: config.get('jwt.expiresIn') },
 				(err, token) => {
-					console.log(err)
 					if (err) {
 						throw err
 					}
-					res.json({ token, user: user.id })
+					res.json({ token, user: user.id, msg: `You are signed up, ${user.username}!` })
 				}
 			)
 		} catch (error) {
 			res.status(500).json({ errors: [{ msg: 'Server error' }] })
 		}
+	}
+)
+
+/* User log in */
+router.post(
+	'/login',
+	[
+		check('email', 'Email is required').notEmpty(),
+		check('password', 'Password is required').notEmpty(),
+	],
+	async (req: Request, res: Response) => {
+		const errors = validationResult(req)
+
+		/* If errors in validation, throw response */
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+
+		const { email, password } = req.body
+
+		const user: IUser | null = await User.findOne({ email })
+
+		/* Checking if user exists */
+		if (!user) {
+			return res.status(400).json({ errors: [{ msg: 'Invalid login credentials' }] })
+		}
+
+		let isValid = bcrypt.compare(password, user.password)
+
+		if (!isValid) {
+			return res.status(400).json({ errors: [{ msg: 'Invalid login credentials' }] })
+		}
+
+		const payload = {
+			user: {
+				id: user.id,
+			},
+		}
+
+		jwt.sign(
+			payload,
+			config.get('jwt.jwtSecret'),
+			{ expiresIn: config.get('jwt.expiresIn') },
+			(err, token) => {
+				if (err) {
+					throw err
+				}
+
+				res.json({ token, user: user.id, msg: `Welcome back, ${user.username}!` })
+			}
+		)
 	}
 )
 
