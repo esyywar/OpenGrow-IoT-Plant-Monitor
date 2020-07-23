ee108b6324fbab438a09ad1247d63fa2c1474a24
esyywar <rahuleswar@gmail.com>
2020-07-22 21:08:22 -0400

    simple landing page

--- application/src/client/src/App.tsx
+++ application/src/client/src/App.tsx
@@ -1,33 +1,30 @@
 import React, { Fragment } from 'react'
 
-import { useSelector } from 'react-redux'
-
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import { Typography } from '@material-ui/core'
 import { Container } from '@material-ui/core'
-import { Alert } from '@material-ui/lab'
 
-import { IAlertState } from './reducers/alerts'
-
+import Alerts from './components/util/Alerts'
 import Navbar from './components/navigation/Navbar'
+import Landing from './components/landing/Landing'
 import Login from './components/userForms/Login'
+import Register from './components/userForms/Register'
 
 function App() {
-	const alerts = useSelector((state: IAlertState) => state.alerts)
-
 	return (
 		<Fragment>
 			<Container maxWidth={false} style={{ padding: '0', minHeight: '100vh' }}>
 				<Router>
 					<Navbar />
-					<Typography
-						component="div"
-						align="center"
-						style={{ backgroundColor: '#F3F8F2', width: '100vw' }}
-					></Typography>
+					<Alerts />
+					<Route exact path="/">
+						<Landing />
+					</Route>
 					<Route path="/login">
 						<Login />
+					</Route>
+					<Route path="/register">
+						<Register />
 					</Route>
 				</Router>
 			</Container>
--- application/src/client/src/components/navigation/Navbar.tsx
+++ application/src/client/src/components/navigation/Navbar.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core/'
+import { createStyles, makeStyles, Theme, useTheme } from '@material-ui/core/styles'
+import { AppBar, Toolbar, Typography, Button, IconButton, Container } from '@material-ui/core/'
 import MenuIcon from '@material-ui/icons/Menu'
 
 import '../../css/navbar.css'
@@ -19,10 +19,30 @@ const useStyles = makeStyles((theme: Theme) =>
 		title: {
 			flexGrow: 1,
 		},
+		linkContainerStyle: {
+			display: 'flexbox',
+			flexDirection: 'row',
+			alignItems: 'center',
+			justifyContent: 'flex-end',
+		},
+		logoBgStyle: {
+			backgroundColor: 'whitesmoke',
+			padding: '5px 8px',
+			borderRadius: '35px',
+		},
+		logoHalfOne: {
+			color: theme.palette.primary.main,
+			fontWeight: 500,
+		},
+		logoHalfTwo: {
+			color: theme.palette.secondary.main,
+			fontWeight: 500,
+		},
 	})
 )
 
 export default function Navbar() {
+	const theme = useTheme()
 	const classes = useStyles()
 
 	return (
@@ -32,14 +52,21 @@ export default function Navbar() {
 					<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
 						<MenuIcon />
 					</IconButton>
-					<Typography variant="h6" className={classes.title}>
-						Ottogrow
-					</Typography>
-					<Button color="inherit">
-						<Link to="/login" className="nav-link">
-							Login
+					<Typography variant="h6">
+						<Link to="/" className="nav-link">
+							<div className={classes.logoBgStyle}>
+								<span className={classes.logoHalfOne}>Otto</span>
+								<span className={classes.logoHalfTwo}>Grow</span>
+							</div>
 						</Link>
-					</Button>
+					</Typography>
+					<Container maxWidth="xl" className={classes.linkContainerStyle}>
+						<Button color="inherit" style={{ float: 'right' }}>
+							<Link to="/login" className="nav-link">
+								Login
+							</Link>
+						</Button>
+					</Container>
 				</Toolbar>
 			</AppBar>
 		</div>
